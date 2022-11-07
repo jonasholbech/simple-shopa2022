925b262961ffd6fc4ebed22b6ccf251a5089201a
Jonas Holbech <jh@jonasholbech.dk>
2022-11-07 16:13:32 +0100

    forgot about branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from "react";
+import Basket from "./components/Basket";
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+
 function App() {
-  return <div className="App"></div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("https://kea-alt-del.dk/t7/api/products");
+      const data = await res.json();
+      setProducts(data);
+    }
+    getData();
+  }, []);
+  return (
+    <div className="App">
+      <Header />
+      <ProductList products={products} />
+      <Basket products={products} />
+    </div>
+  );
 }
 
 export default App;
